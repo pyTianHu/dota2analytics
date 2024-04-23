6d8e768e37e26b1f083259fad4876a796819dd43
Todik <Krisztian.Todik@altrata.com>
2024-04-24 00:42:24 +0200

    DOT-5 creating drop table in tableoperations

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,5 +1,9 @@
 import sqlite3
 import pandas as pd
+import sys
+sys.path.append('../dota2')
+
+from classes.tableoperations import TableOperations
 
 def test_case_d10(db_name, table_name):
     conn = sqlite3.connect(db_name)
@@ -11,4 +15,44 @@ def test_case_d10(db_name, table_name):
 
     print(df)
 
-test_case_d10('dot_raw_prod.db', 'heroes')
+#test_case_d10('dot_raw_prod.db', 'heroes')
+
+def test_case_dot5(db_name, table_name):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    
+    #create test table
+    schema = ('id INTEGER NOT NULL, name TEXT NULL')
+    test_table = TableOperations(db_name, table_name)
+    test_table.create_table()
+
+    #check if table exists
+    check_table_query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+    cursor.execute(check_table_query)
+
+    # Fetch the result
+    result = cursor.fetchone()
+
+    # Check if the table exists
+    if result:
+        print(f"The table '{table_name}' exists in the database.")
+    else:
+        print(f"The table '{table_name}' does not exist in the database.")
+
+    #drop table
+    test_table.drop_table()
+
+    #check if table exists
+    check_table_query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+    cursor.execute(check_table_query)
+
+    # Fetch the result
+    result = cursor.fetchone()
+
+    # Check if the table exists
+    if result:
+        print(f"The table '{table_name}' exists in the database.")
+    else:
+        print(f"The table '{table_name}' does not exist in the database.")
+
+test_case_dot5('dot_dev_db.db','test_table')
