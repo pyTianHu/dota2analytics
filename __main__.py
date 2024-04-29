1977e10364fac86a2489b389946fe60f47039b8c
Todik <Krisztian.Todik@altrata.com>
2024-04-29 22:58:23 +0200

    Adding some draft to main.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,10 @@
+from scripts.job import table_create_and_ingest
+from utils.utils import table_function_mapping
+
 def main():
+    #table_create_and_ingest() => repeated for all data sources for each in table_sources_mapping
+    #for table, func in table_function_mapping:
+    #    table_create_and_ingest(table)
     pass
 
 if __name__ == "__main__":
