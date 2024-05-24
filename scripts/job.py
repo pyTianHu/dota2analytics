3319a56e883b1dc95fb96857d0c4eb5fc89ed516
Todik <Krisztian.Todik@altrata.com>
2024-05-24 10:20:47 +0200

    Removing logger function temporarily 2nd batch

--- scripts/job.py
+++ scripts/job.py
@@ -11,9 +11,6 @@ from utils.ingestion_utils import table_function_mapping
 from utils.gold_utils import table_constraints, table_rename
    
 def table_create_and_ingest(db_name, table_name):
-    #call logger method with function name and df => source db, table => nothing, as no source yet, just call function to record start time
-    tci = logger(f"{table_create_and_ingest.__name__} function started", f"{table_create_and_ingest.__name__}")
-    tci.new_or_existing_run()
     #calling and executing ingestion function, storing it in df variable
     df = table_function_mapping.get(table_name)
 
@@ -38,20 +35,15 @@ def table_create_and_ingest(db_name, table_name):
     # return has to be edited, as it returns invalid result. function runs successfully and that is what it returns.
     try:
         ingested_table.insert_df_into_table()
+        return True
     except Exception as e:
-        #call logger with function name and no data was inserted {e} exception
-        ndi = logger(f"{TableOperations.insert_df_into_table.__name__} function finished, no data was inserted into {table_name}")
-        tci.new_or_existing_run()
-    
-    tci2 = logger("table_create_and_ingest function finished", "table_create_and_ingest")
-    tci2.new_or_existing_run
+        return e
 
 
 def bronze_transformation(raw_db_name, table_name, bronze_db_name):
     # data source: raw layer; output: bronze layer => dot_dev_bronze.db for dev env, dot_bronze.db for prod
     # dropping unnecessary columns & selecting only necessary ones
-    bt = logger(f"{bronze_transformation.__name__} function started", f"{bronze_transformation.__name__}")
-    bt.new_or_existing_run()
+    
 
     #get selected cols from utils
     # if selected cols is empty, pass
@@ -70,9 +62,6 @@ def bronze_transformation(raw_db_name, table_name, bronze_db_name):
     #insert into bronze table
     df_to_table = TableOperations(bronze_db_name, table_name, data = df)
     res = df_to_table.insert_df_into_table()
-
-    bt2 = logger(f"{bronze_transformation.__name__} function finished: result of insert_df_into_table function => {res}", f"{bronze_transformation.__name__}")
-    bt2.new_or_existing_run()
 
     return res
 
